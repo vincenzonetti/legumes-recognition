44b924598e3648255cc04aa7f5c03114aa489e00
Vincenzo <vincenzontt02@gmail.com>
2025-01-02 15:53:21 +0100

    added display functions

--- svm_train.py
+++ svm_train.py
@@ -39,23 +39,44 @@ def TSNE_plot_data(X_train, y_train):
     labels = set(y_train)
     colors = plt.cm.viridis(np.linspace(0, 1, len(labels)))
     for i, label in enumerate(labels):
-        plt.scatter(X_embedded[y_train == label, 0], X_embedded[y_train == label, 1], c=colors[i], label=label)
+        plt.scatter(X_embedded[y_train == label, 0], X_embedded[y_train == label, 1], color=colors[i], label=label)
     plt.legend()
     plt.axis('off')
     plt.title('TSNE plot of training data')
-    plt.show()  
+    plt.show()
+
+def PCA_plot_data(X_train, y_train):
+    pca = PCA(n_components=2)
+    X_train = pca.fit_transform(X_train)
+    labels = set(y_train)
+    colors = plt.cm.viridis(np.linspace(0, 1, len(labels)))
+    for i, label in enumerate(labels):
+        plt.scatter(X_train[y_train == label, 0], X_train[y_train == label, 1], color=colors[i], label=label)
+    plt.legend()
+    plt.axis('off')
+    plt.title('PCA plot of training data')
+    plt.show()
+
+def plot_decision_boundaries(X_train, y_train, svm):
+    pca = PCA(n_components=2)
+    X_train = pca.fit_transform(X_train)
+    svm.fit(X_train, y_train)
+    DecisionBoundaryDisplay.from_estimator(svm, X_train)
+    plt.show()
+
 
 if __name__ == '__main__':
     df = pd.read_csv('features.csv')
     feature_vectors = df.drop('label', axis=1).values
     labels = df['label'].values
-    USE_PCA = False
+    USE_PCA = True
+    PCA_COMPONENTS = 5
     X = np.array(feature_vectors)  # Feature vectors
     y = np.array(labels)  # Labels for the seeds
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     kernel = 'linear'
     if USE_PCA:
-        pca = PCA(n_components=2)
+        pca = PCA(n_components=PCA_COMPONENTS)
         X_train = pca.fit_transform(X_train)
         X_test = pca.transform(X_test)
     
@@ -63,7 +84,9 @@ if __name__ == '__main__':
     #svm = joblib.load('svm_model.pkl')
     accuracy = svm.score(X_test, y_test)
     print(f'Accuracy: {accuracy}')
-    #plot_confusion_matrix(y_test, X_test, svm)
-    TSNE_plot_data(X_train,y_train)
+    plot_confusion_matrix(y_test, X_test, svm)
+    #TSNE_plot_data(X_train,y_train)
+    #PCA_plot_data(X_train,y_train)
+    plot_decision_boundaries(X_train, y_train, svm)
     
     joblib.dump(svm, 'svm_model.pkl')

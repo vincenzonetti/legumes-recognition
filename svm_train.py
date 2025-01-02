ba0cd670f9b1499c886780685249ec1d9235a102
Vincenzo <vincenzontt02@gmail.com>
2025-01-02 15:42:04 +0100

    added tsne visualization and pca

--- svm_train.py
+++ svm_train.py
@@ -7,6 +7,12 @@ import pandas as pd
 from feature_extraction import extract_feature
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.manifold import TSNE
+
+
+
 import random
 import os
 import joblib
@@ -23,23 +29,41 @@ def plot_confusion_matrix(y_true, X_test, svm):
     plt.yticks(range(len(set(y_true))), set(y_true))
     plt.show()
 
-#load dataframe
+def train_svm(X_train, y_train, kernel='linear'):
+    svm = make_pipeline(StandardScaler(), SVC(kernel=kernel))
+    svm.fit(X_train, y_train)
+    return svm
+
+def TSNE_plot_data(X_train, y_train):
+    X_embedded = TSNE(n_components=2).fit_transform(X_train)
+    labels = set(y_train)
+    colors = plt.cm.viridis(np.linspace(0, 1, len(labels)))
+    for i, label in enumerate(labels):
+        plt.scatter(X_embedded[y_train == label, 0], X_embedded[y_train == label, 1], c=colors[i], label=label)
+    plt.legend()
+    plt.axis('off')
+    plt.title('TSNE plot of training data')
+    plt.show()  
+
 if __name__ == '__main__':
     df = pd.read_csv('features.csv')
     feature_vectors = df.drop('label', axis=1).values
     labels = df['label'].values
-    
+    USE_PCA = False
     X = np.array(feature_vectors)  # Feature vectors
     y = np.array(labels)  # Labels for the seeds
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    kernel = 'linear'
+    if USE_PCA:
+        pca = PCA(n_components=2)
+        X_train = pca.fit_transform(X_train)
+        X_test = pca.transform(X_test)
     
-    # Normalize features and train SVM
-    svm = make_pipeline(StandardScaler(), SVC(kernel='linear'))
-    svm.fit(X_train, y_train)
-    
-    
+    svm = train_svm(X_train, y_train,kernel=kernel)
+    #svm = joblib.load('svm_model.pkl')
     accuracy = svm.score(X_test, y_test)
     print(f'Accuracy: {accuracy}')
-    plot_confusion_matrix(y_test, X_test, svm)
+    #plot_confusion_matrix(y_test, X_test, svm)
+    TSNE_plot_data(X_train,y_train)
     
     joblib.dump(svm, 'svm_model.pkl')
